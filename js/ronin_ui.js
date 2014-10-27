b7a5d3f31630223fb1334c866e3697ec82af4652
Milan Petrovic <micko.kv@gmail.com>
2014-10-27 13:52:06 +0100

    Search scrollable list done

--- js/ronin_ui.js
+++ js/ronin_ui.js
@@ -6,4 +6,17 @@ $(function(){
       swipeLeft: function() { $(this).find('div').removeClass('show') }
     });
   }
+  if(window.IScroll) {
+    $('.searchList div[id][data-scroll]').each(function(){
+      var id = $(this).attr('id'),
+          size = $(this).attr('data-scroll');
+
+      $(this).attr('style', 'height:'+size+'px;');
+
+      $(window).on('load', function() {
+        new IScroll('#'+id, { freescroll: true, scrollbars: true });
+      });
+
+    });
+  }
 });
